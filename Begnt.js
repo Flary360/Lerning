9294f7f75085f344ecf5953f4a4597bb33c88460
Andreas Kjellander <andreas.kjellander1@learnet.se>
2022-08-26 15:42:33 +0200

    Tror jag gjort rätt

---
+++ Begnt.js
@@ -0,0 +1,43 @@
+/*
+// första test
+console.log("hej")  // Sträng
+console.log(2) // Number
+console.log(true) //boolean
+console.log(false) // boolean
+
+//test fail
+console.log(2+2);
+console.log(4-1);
+console.log(" that's 3 quick math")
+console.log(1*3);
+console.log(3/2);
+
+
+var UserName = "Andreas"
+console.log(UserName)
+UserName = "Begnt"
+console.log(UserName)
+
+
+//uppgift
+console.log("Hello World")
+console.log("I have been here from the biging.\n I just want to die.\n Can you help?.")
+
+
+var ps = require("prompt-sync");
+var prompt = ps();
+
+let name = prompt("Enter your name: ");
+// Write your code here:
+
+console.log(name)
+*/
+
+var ps = require("prompt-sync");
+var prompt =ps();
+
+let name = prompt("Enter Hello: ")
+console.log(name)
+
+
+
