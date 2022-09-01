d09b4ade1f09a7b5f7b9e4b82c5fd936d89ff5d3
Andreas Kjellander <andreas.kjellander1@learnet.se>
2022-09-01 11:05:46 +0200

    bra

--- Begnt.js
+++ Begnt.js
@@ -31,7 +31,7 @@ let name = prompt("Enter your name: ");
 // Write your code here:
 
 console.log(name)
-*/
+
 
 var ps = require("prompt-sync");
 var prompt =ps();
@@ -39,5 +39,58 @@ var prompt =ps();
 let name = prompt("Enter Hello: ")
 console.log(name)
 
+var ps = require("prompt-sync");
+var prompt =ps();
+
+let name = prompt("What's your name?: ")
+console.log(name)
+console.log(name)
+console.log(name)
 
 
+var ps = require("prompt-sync");
+var prompt = ps();
+
+let name = prompt("what's your name?: ")
+console.log("Hello " + name + "!")
+
+//98 (C)(TM)(R)
+var ps = require("prompt-sync")
+var prompt = ps()
+
+let q1 = prompt("Hello, how are you?: ")
+
+
+if ((q1 == "Fine, thanks") || (q1 == "fine, thanks")) {
+
+let q2 = prompt("That's intresting, tell me more: ") 
+if ((q2 == "I learn coding") || (q2 == "i learn coding")) {
+
+
+console.log("Thank you for sharing!")
+}
+ else {
+    console.log ("why u gay?")
+} 
+
+} 
+else {
+    console.log ("why u gay?")
+} 
+
+
+//10
+var ps = require("prompt-sync");
+var prompt = ps();
+
+console.log("I will tell you a story, bit i need some information first.")
+let name = prompt("Give a name for the main character: ")
+
+
+let work = prompt("Give the character a profession: ")
+
+
+console.log("Here is the story: \nOnce upon a time there was a", work, "called" , name + ". On there way to work,",name, "often pondered what being", work, "meant to them. When you work as a" , work, "you meet intresting people." ,name, "enjoys their work as" ,work + ", The end" )
+*/
+
+jm
